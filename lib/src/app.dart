8bd6d5661959646dfa9da3c3f8257b32ce9879d3
m01310g <m01310g@gmail.com>
2024-03-31 01:38:59 +0900

    test

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import 'package:travelmaker/pages/community_page.dart';
 import 'package:travelmaker/pages/home_page.dart';
 import 'package:travelmaker/pages/post/plan_page.dart';
-import 'package:travelmaker/pages/regioninfo_page.dart';
+import 'package:travelmaker/pages/region_info_page.dart';
 import 'package:travelmaker/pages/post/my_page.dart';
 import 'package:travelmaker/src/components/image_data.dart';
 import 'package:travelmaker/src/controller/bottom_nav_controller.dart';
