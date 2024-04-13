29671227a5889f850e08c9a4cbaefc45d18478ae
zsuant <lsg001025@gmail.com>
2024-04-13 23:03:25 +0900

    api

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,35 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get.dart';
+import 'package:travelmaker/pages/post/plan_page.dart';
 import 'package:travelmaker/src/app.dart';
 import 'package:travelmaker/src/binding/init_binding.dart';
 
-void main() {
+// 지도 초기화하기
+Future<void> _initialize() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapSdk.instance.initialize(
+    clientId: 'vxoaja5vyl', // 클라이언트 ID 설정
+    onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed"),
+  );
+}
+
+void main() async {
+  // 지도 초기화 함수 호출
+  await _initialize();
+  // 앱 실행
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-
-      // controller를 instance로 설정
       initialBinding: InitBinding(),
       home: const App(),
     );
