0d141a3b63dcb5c9b65abe9fc7a87a5583d58611
kjmg7979 <kjmg7979@naver.com>
2024-05-11 19:44:55 +0900

    안올라갔던애들

--- lib/pages/post/my_page.dart
+++ lib/pages/post/my_page.dart
@@ -1,15 +1,14 @@
-// my_page.dart
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart'; // Firebase Storage 추가
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../firebase/login_google.dart';
 import '../../firebase/user_data.dart';
 import 'my_liked_list.dart';
 import 'my_post_list.dart';
-import 'package:google_sign_in/google_sign_in.dart'; // GoogleSignIn 추가
-import 'package:firebase_auth/firebase_auth.dart'; // FirebaseAuth 추가
-import 'package:travelmaker/firebase/login_google.dart'; // 구글 로그인 페이지 추가
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class UserModel {
   String profileImagePath;
@@ -123,11 +122,57 @@ class _MyPageState extends State<MyPage> {
   void _pickImage() async {
     final XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
+      File imageFile = File(pickedFile.path);
+
+      // 이미지의 크기를 확인
+      int fileSize = await imageFile.length();
+      double fileSizeInKB = fileSize / 1024; // 파일 크기를 KB로 변환
+
+      // 파일 크기가 일정 이상인 경우 경고 표시
+      if (fileSizeInKB > 1024) { // 예를 들어, 1024KB(1MB) 이상인 경우
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('이미지 크기가 너무 큽니다'),
+              content: Text('사진 크기가 맞지 않을 수 있습니다. 계속해서 업로드 하시겠습니까?'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    _uploadImage(imageFile); // 사용자가 계속해서 업로드를 선택한 경우 이미지 업로드 수행
+                  },
+                  child: Text('예'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('아니오'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        // 파일 크기가 적절한 경우에는 그냥 이미지 업로드 수행
+        _uploadImage(imageFile);
+      }
+    }
+  }
+
+  void _uploadImage(File imageFile) async {
+    // Firebase Storage에 이미지 업로드
+    Reference storageReference = FirebaseStorage.instance.ref().child('profile_images/${DateTime.now().millisecondsSinceEpoch}');
+    UploadTask uploadTask = storageReference.putFile(imageFile);
+    await uploadTask.whenComplete(() async {
+      // 업로드가 완료되면 이미지 다운로드 URL을 가져와서 업데이트
+      String imageUrl = await storageReference.getDownloadURL();
       setState(() {
         // 프로필 이미지 경로를 업데이트
-        user.profileImagePath = pickedFile.path;
+        user.profileImagePath = imageUrl;
       });
-    }
+    });
   }
 
   // 로그아웃 기능
